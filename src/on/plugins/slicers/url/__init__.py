4ed4d7f78fa08cd0e8e3c649b89150f00fefc1c0
Sebastien Tricaud <sebastien.tricaud@gmail.com>
2012-08-28 09:11:38 +0200

    Return a string from a buffer

--- src/on/plugins/slicers/url/__init__.py
+++ src/on/plugins/slicers/url/__init__.py
@@ -15,43 +15,43 @@ def slice_buffer(slicer, buffer_chunk, pdata):
     for column in slicer['columns']:
         for tag in column['tags']:
             if tag == "http-credential":
-                retval.append(url['credential'])
+                retval.append(str(url['credential']))
                 n_events += 1
                 continue
             if tag == "http-domain":
-                retval.append(url['domain'])
+                retval.append(str(url['domain']))
                 n_events += 1
                 continue
             if tag == "http-subdomain":
-                retval.append(url['subdomain'])
+                retval.append(str(url['subdomain']))
                 n_events += 1
                 continue
             if tag == "http-fragment":
-                retval.append(url['fragment'])
+                retval.append(str(url['fragment']))
                 n_events += 1
                 continue
             if tag == "http-host":
-                retval.append(url['host'])
+                retval.append(str(url['host']))
                 n_events += 1
                 continue
             if tag == "http-resource_path":
-                retval.append(url['resource_path'])
+                retval.append(str(url['resource_path']))
                 n_events += 1
                 continue
             if tag == "http-tld":
-                retval.append(url['tld'])
+                retval.append(str(url['tld']))
                 n_events += 1
                 continue
             if tag == "http-query_string":
-                retval.append(url['query_string'])
+                retval.append(str(url['query_string']))
                 n_events += 1
                 continue
             if tag == "http-scheme":
-                retval.append(url['scheme'])
+                retval.append(str(url['scheme']))
                 n_events += 1
                 continue
             if tag == "http-port":
-                retval.append(url['port'])
+                retval.append(str(url['port']))
                 n_events += 1
                 continue
     
